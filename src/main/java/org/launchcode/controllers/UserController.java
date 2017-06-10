8ba1699411ed2d441c26aaa22b49130ab5f16440
Andrew Mihalevich <apmihal@gmail.com>
2017-06-10 07:55:02 -0500

    Added User class, signup form, welcome screen, and error handling for mismatched passwords.

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,7 +1,39 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by AndrewM on 6/10/2017.
  */
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, User user, String verify) {
+
+        if (user.getPassword().equals(verify)) {
+            model.addAttribute("username", user.getUsername());
+
+            return "user/index";
+
+        } else {
+
+            boolean error = true;
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("error", error);
+
+            return "user/add";
+        }
+    }
 }
