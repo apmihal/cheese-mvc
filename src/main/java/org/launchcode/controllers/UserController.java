282bbf5b0eebc895f61f26aa2606202915157d29
Andrew Mihalevich <apmihal@gmail.com>
2017-06-10 07:06:46 -0500

    Initial commit.

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,7 @@
+package org.launchcode.controllers;
+
+/**
+ * Created by AndrewM on 6/10/2017.
+ */
+public class UserController {
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,7 @@
+package org.launchcode.models;
+
+/**
+ * Created by AndrewM on 6/10/2017.
+ */
+public class User {
+}
